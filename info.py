55e9c9cd5762abfac7d2fb0c69274925434b2e1f
AJBOTS41 <sivasaiajay06@gmail.com>
2025-01-11 16:52:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,9 +88,9 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/vj_bot_disscussion')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/vj_botz')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/tfirequestgroup')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/tfi_oficial')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'tfirequestgroup') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/tfi_oficial')
 
 # True Or False
